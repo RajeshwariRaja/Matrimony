227cd8b67b3782b0d770a8782815f5bf43c3ec7f
rajeshwariraja <rajimca2017@gmail.com>
2017-09-20 08:23:24 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ClarityModule } from "clarity-angular";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ClarityModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
